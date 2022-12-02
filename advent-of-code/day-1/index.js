8d044659c70d8d2014fc22123031e2c3565cbf26
wtitterton <wftitterton@gmail.com>
2022-12-02 12:59:45 +0100

    Add files via upload

--- advent-of-code/day-1/index.js
+++ advent-of-code/day-1/index.js
@@ -4,40 +4,54 @@ const readline = require('readline');
 const readStream = fs.createReadStream('./elf-calories.txt', 'utf-8');
 let rl = readline.createInterface({input: readStream});
 
-let currentCaloryCount = 0;
-let currentElfCount = 0;
-let elfCalories = [];
+const readFileContents = (cb) => {
+   let currentCaloryCount = 0;
+   let currentElfCount = 0;
+   let elves = [];
+    rl.on('line', (line) => {
+        if (line.length === 0) {
+            currentElfCount++;
+            elves.push({
+                id: currentElfCount,
+                caloryCount: currentCaloryCount
+            })
+            currentCaloryCount = 0;
+            return;
+        }
 
- rl.on('line', (line) => {
-     if (line.length === 0) {
-        currentElfCount++;
-
-        elfCalories.push({
-            id: currentElfCount,
-            caloryCount: currentCaloryCount
-        })
-        currentCaloryCount = 0;
-        return;
-    }
-
-     currentCaloryCount = currentCaloryCount + Number.parseInt(line, 10);
-   });
-
-   rl.on('close', function() {
-    elfCalories.sort(function(a, b) {
-        return b.caloryCount - a.caloryCount 
+        currentCaloryCount = currentCaloryCount + Number.parseInt(line, 10);
     });
-    const topThreeElves = elfCalories.slice(0, 3);
 
-    topThreeElves.forEach(({id, caloryCount}) => {
-        console.log(`elf ${id} has ${caloryCount} calories`);
+    rl.on('close', () => {
+      cb(elves);
     })
+}
 
-    const totalCalories = topThreeElves.reduce((accumulator, currentValue) => {
+const getTopThreeElves = (elves) => {
+    return elves.sort(function(a, b) {
+        return b.caloryCount - a.caloryCount 
+    }).slice(0, 3);  
+}
+
+const getTotalCalories = (elves) => {
+    return elves.reduce((accumulator, currentValue) => {
         return accumulator + currentValue.caloryCount
     }, 0)
+}
 
-    console.log(totalCalories);
-   })
+const printElf = ({id, caloryCount}) => {
+  console.log(`elf ${id} has ${caloryCount} calories`);
+}
+
+const getResults = (elves) => {
+    const topThreeElves = getTopThreeElves(elves);
+    console.log(`The top three elves are carrying a total of ${getTotalCalories(topThreeElves)} calories`)
+    topThreeElves.forEach(printElf);
+}
+
+readFileContents(getResults);
+
+
+
 
    
